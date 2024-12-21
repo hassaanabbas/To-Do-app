a024ff1a2e67c237dc3c1287e0a93e58d0d87a4f
hassaanabbas <hassaanabbass987@gmail.com>
2024-12-21 17:37:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if (inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        const li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "",
+    SAVEDATA();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+})
+
+function SAVEDATA(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
